7c4fb0ebf89c13be787e3e2a8cd756d813d95496
johney <johneyburke@gmail.com>
2023-09-08 21:42:04 -0600

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let someValue;
+
+function scuberGreetingForFeet(someValue){
+  
+  if (someValue < 401){
+    return  'This one is on me!';
+
+}
+  else if (someValue > 400 && someValue < 2001){
+
+    return  'That will be twenty bucks.';
+}
+  else if (someValue > 2000 && someValue < 2500 ){
+
+  return  'I will gladly take your thirty bucks.';
+}
+  else return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city
+function ternaryCheckCity(city){
+   let greeting = city === 'NYC'?  'Ok, sounds good.' :  "No go." 
+    return greeting;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+//let tip;
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous' : return 'Thank you so much.';
+    case 'not as generous' : return 'Thank you.';
+    default : return 'Bye.';
+
+  }
 }
